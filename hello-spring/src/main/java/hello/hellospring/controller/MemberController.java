65f2829bdb16e5a9e5aae1e680bfba0a114905f2
SeungHan <jk05018@naver.com>
2022-01-05 19:37:47 +0900

    feat(MemberController, memberList.html) : member 추가 및 조회 기능 추가

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,9 +1,15 @@
 package hello.hellospring.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import hello.hellospring.domain.Member;
+import hello.hellospring.dto.MemberDto;
 import hello.hellospring.service.MemberService;
 
 @Controller
@@ -19,5 +25,21 @@ public class MemberController {
 	@GetMapping("/members/new")
 	public String createForm(){
 		return "members/createMemberForm";
+	}
+
+	@PostMapping("/members/new")
+	public String create(MemberDto memberDto){
+		Member member = new Member();
+		member.setName(memberDto.getName());
+
+		memberService.join(member);
+		return "redirect:/";
+	}
+
+	@GetMapping("/members")
+	public String list(Model model){
+		List<Member> members = memberService.findMembers();
+		model.addAttribute("members",members);
+		return "members/memberList";
 	}
 }
