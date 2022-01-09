db0bdf4de8ca881cebc0d214778d4f816b6fe8f7
SeungHan <jk05018@naver.com>
2022-01-09 15:30:47 +0900

    feat : jpa proxy 실습 시작

---
+++ jpa_proxy/src/main/java/me/develop_han/jpa_proxy/JpaProxyApplication.java
@@ -0,0 +1,13 @@
+package me.develop_han.jpa_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaProxyApplication.class, args);
+	}
+
+}
---
+++ jpa_proxy/src/test/java/me/develop_han/jpa_proxy/JpaProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.develop_han.jpa_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
