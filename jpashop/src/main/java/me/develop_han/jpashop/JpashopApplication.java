da1d03cd84c83ef7f351a5ca95111d51f7dca61b
SeungHan <jk05018@naver.com>
2022-01-10 21:27:04 +0900

    feat : jpashop 프로젝트 실습 start

---
+++ jpashop/src/main/java/me/develop_han/jpashop/JpashopApplication.java
@@ -0,0 +1,13 @@
+package me.develop_han.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+}
