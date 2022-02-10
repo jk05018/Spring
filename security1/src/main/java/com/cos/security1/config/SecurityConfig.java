efeeb17358791ddca15fa0e6016ccdcf055cf40d
SeungHan <jk05018@naver.com>
2022-02-10 21:34:24 +0900

    feat : security 기본 세팅 및 로그인, 회원가입

---
+++ security1/src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.cos.security1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity(debug = true) // 스프링 시큐리티 필터가 스프링 필터 체인에 등록이 된다.
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.csrf().disable()
+			.authorizeRequests().antMatchers("/user/**").authenticated()
+			.antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')") // 무슨 언어 있댔음 패캠 강의 듣기
+			.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')") // 403 접근 권한 없
+			.anyRequest().permitAll()
+			.and().formLogin().loginPage("/loginForm")
+		;
+
+	}
+}
