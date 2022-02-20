48c0b80a48c5182a0f1e4a6f2a84f5b4daf0a627
SeungHan <jk05018@naver.com>
2022-02-20 20:04:55 +0900

    security 파일 정리

--- security1/revise/src/main/java/com/cos/LoginApplication.java
+++
@@ -1,2 +0,0 @@
-package com.cos;public class LoginApplication {
-}
--- security_jwt/src/main/java/me/seunghan/security_jwt/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package me.seunghan.security_jwt.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import me.seunghan.security_jwt.model.User;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-	User findByUsername(String username);
-}
