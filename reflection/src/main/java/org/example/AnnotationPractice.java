50b37687c0199e1bcab9283d3609cc8e4802d4d2
SeungHan <jk05018@naver.com>
2022-02-28 20:47:00 +0900

    di 동작 원리 복습

--- reflection/src/main/java/org/example/AnnotationPractice.java
+++ reflection/src/main/java/org/example/AnnotationPractice.java
@@ -9,7 +9,8 @@ public class AnnotationPractice {
 		Class<AnnotationPractice> annotationPracticeClass = AnnotationPractice.class;
 		Arrays.stream(annotationPracticeClass.getAnnotations()).forEach(System.out::println);
 
-		System.out.println("=====================================");;
+		System.out.println("=====================================");
+		;
 		// getAnnotations를 사용한다면 해당되는 모든 annotation을 가져오고
 		Arrays.stream(MyBook.class.getAnnotations()).forEach(System.out::println);
 		//만약 이 클래스에 선언되어  있는 annotation만 가져오고 싶다면 아래 declared를 사용하면 된다.
--- reflection/src/main/java/org/example/di/ContainerService.java
+++ reflection/src/main/java/org/example/di/ContainerService.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class ContainerService {
 
 	// 제너릭스 잘 이해해 보기
-	public static <T> T getObject(Class<T> classType){
+	public static <T> T getObject(Class<T> classType) {
 		T instance = createInstance(classType);
 		Arrays.stream(classType.getDeclaredFields()).forEach(f -> {
 			if (f.getAnnotation(Inject.class) != null) {
@@ -22,7 +22,7 @@ public class ContainerService {
 		return instance;
 	}
 
-	private static <T> T createInstance(Class<T> classType){
+	private static <T> T createInstance(Class<T> classType) {
 		try {
 			return classType.getConstructor(null).newInstance();
 		} catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
