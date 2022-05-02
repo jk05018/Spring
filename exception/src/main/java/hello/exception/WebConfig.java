9dd428ff69f482e258501ec19168758b187d1bc7
SeungHan <jk05018@naver.com>
2022-05-02 17:13:59 +0900

    exception resolver로 가장 단순하게 처리하기

--- exception/src/main/java/hello/exception/WebConfig.java
+++ exception/src/main/java/hello/exception/WebConfig.java
@@ -1,16 +1,21 @@
 package hello.exception;
 
+import java.util.List;
+
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import hello.exception.filter.LogFilter;
 import hello.exception.intercepter.LogInterceptor;
+import hello.exception.resolver.MyHandlerExceptionResolver;
+import hello.exception.resolver.UserHandlerExceptionResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -21,6 +26,12 @@ public class WebConfig implements WebMvcConfigurer {
 		.order(1)
 		.addPathPatterns("/**")
 		.excludePathPatterns("/css/**","*/ico","/error","/error-page/**"); // 오류 페이지 경로를 넣어줌?
+	}
+
+	@Override
+	public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+		resolvers.add(new MyHandlerExceptionResolver());
+		resolvers.add(new UserHandlerExceptionResolver());
 	}
 
 	// @Bean
