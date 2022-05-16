70efef432bcdcfec6cbd950447991ee5e7b21540
SeungHan <jk05018@naver.com>
2022-05-16 12:27:34 +0900

    string data jpa query method 실습

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,8 +1,13 @@
 package study.datajpa.repository;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -14,4 +19,24 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
 	@Query("select m from Member m where m.username = :username and m.age = :age")
 	List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+	List<Member> findListByUsername(String username);
+	Member findMemberByUsername(String username);
+
+	Page<Member> findByAge(int age, Pageable pageable);
+
+	@Modifying(clearAutomatically = true)
+	@Query("update Member m set m.age = m.age + 1 where m.age >= :age")
+	int buldAgeUpdtae(@Param("age") int age);
+
+	@Override
+	@EntityGraph(attributePaths = {"team"})
+	List<Member> findAll();
+
+	@EntityGraph(attributePaths = {"team"})
+	@Query("select m from Member m")
+	List<Member> findMemberByEntityGraph();
+
+	@EntityGraph(attributePaths = {"team"})
+	List<Member> findByUsername(String username);
 }
--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -4,11 +4,14 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 import study.datajpa.entity.Member;
 
@@ -16,28 +19,31 @@ import study.datajpa.entity.Member;
 @Transactional
 class MemberRepositoryTest {
 	@Autowired
-	MemberRepository memberRe;
+	MemberRepository memberRepository;
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void basicCRUD() {
 		final Member member1 = new Member("member1");
 		final Member member2 = new Member("member2");
 
-		memberRe.save(member1);
-		memberRe.save(member2);
+		memberRepository.save(member1);
+		memberRepository.save(member2);
 
-		final Member findMember1 = memberRe.findById(member1.getId()).get();
-		final Member findMember2 = memberRe.findById(member2.getId()).get();
+		final Member findMember1 = memberRepository.findById(member1.getId()).get();
+		final Member findMember2 = memberRepository.findById(member2.getId()).get();
 		assertThat(findMember1).isEqualTo(member1);
 		assertThat(findMember2).isEqualTo(member2);
 
-		final List<Member> all = memberRe.findAll();
+		final List<Member> all = memberRepository.findAll();
 		assertThat(all.size()).isEqualTo(2);
 
-		memberRe.delete(member1);
-		memberRe.delete(member2);
+		memberRepository.delete(member1);
+		memberRepository.delete(member2);
 
-		final List<Member> deletedCount = memberRe.findAll();
+		final List<Member> deletedCount = memberRepository.findAll();
 		assertThat(deletedCount.size()).isEqualTo(0);
 
 
@@ -48,10 +54,10 @@ class MemberRepositoryTest {
 		final Member member1 = new Member("AAA",10);
 		final Member member2 = new Member("AAA",20);
 
-		memberRe.save(member1);
-		memberRe.save(member2);
+		memberRepository.save(member1);
+		memberRepository.save(member2);
 
-		final List<Member> result = memberRe.findByUsernameAndAgeGreaterThan("AAA", 15);
+		final List<Member> result = memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
 
 		assertThat(result.get(0).getUsername()).isEqualTo("AAA");
 		assertThat(result.get(0).getAge()).isEqualTo(20);
@@ -64,10 +70,10 @@ class MemberRepositoryTest {
 		final Member member1 = new Member("AAA",10);
 		final Member member2 = new Member("AAA",20);
 
-		memberRe.save(member1);
-		memberRe.save(member2);
+		memberRepository.save(member1);
+		memberRepository.save(member2);
 
-		final List<Member> result = memberRe.findUser("AAA", 10);
+		final List<Member> result = memberRepository.findUser("AAA", 10);
 
 		assertThat(result.get(0).getUsername()).isEqualTo("AAA");
 		assertThat(result.get(0).getAge()).isEqualTo(10);
@@ -75,4 +81,56 @@ class MemberRepositoryTest {
 
 	}
 
+	@Test
+	void paging() {
+		memberRepository.save(new Member("member1", 10));
+		memberRepository.save(new Member("member2", 10));
+		memberRepository.save(new Member("member3", 10));
+		memberRepository.save(new Member("member4", 10));
+		memberRepository.save(new Member("member5", 10));
+
+		int age = 10;
+		int page = 0;
+		int size = 3;
+
+		final PageRequest pageRequest = PageRequest.of(page, size);
+
+		final Page<Member> pageByAge = memberRepository.findByAge(age, pageRequest);
+
+	}
+
+	@Test
+	void bulkupdate() {
+		memberRepository.save(new Member("member1", 10));
+		memberRepository.save(new Member("member2", 19));
+		memberRepository.save(new Member("member3", 20));
+		memberRepository.save(new Member("member4", 21));
+		memberRepository.save(new Member("member5", 40));
+
+		final List<Member> members = memberRepository.findAll();
+
+		for (Member member : members) {
+			System.out.println("member : " + member);
+		}
+
+		final int i = memberRepository.buldAgeUpdtae(20);
+
+		final List<Member> member1 = memberRepository.findAll();
+
+		for (Member member : member1) {
+			System.out.println("member : " + member);
+		}
+
+		assertThat(i).isEqualTo(3);
+
+		em.flush();
+		em.clear();
+
+		final List<Member> members2 = memberRepository.findAll();
+
+		for (Member member : members2) {
+			System.out.println("member2: " + member);
+		}
+
+	}
 }
